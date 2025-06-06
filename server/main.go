7589ef77683af7aabb2583ce52a5a4dd5da84fb5
J. Hinchliffe <jjh413@student.bham.ac.uk>
2025-06-06 23:47:21 +0100

    server: refactor main function
    It was getting a little big so we now have smaller functions to handle init.

--- server/main.go
+++ server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,37 +12,56 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/postgres"
 	"github.com/gin-contrib/static"
-
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-func main() {
+var (
+	postgresUri string = ""
+	staticDir   string = ""
+)
+
+type AppState struct {
+	// Shared connection to the database, used for adding temporary emails as well as handling sessions.
+	db *sql.DB
+}
+
+// Loads the environment variables from the `.env` file.
+func loadEnvVars() error {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Printf("Error loading .env file: %v", err)
-		return
+		return fmt.Errorf("loading .env file failed: %v", err)
 	}
 
-	postgresUri := os.Getenv("POSTGRES_URI")
+	postgresUri = os.Getenv("POSTGRES_URI")
 	if postgresUri == "" {
-		log.Printf("POSTGRES_URI not set, please set it in your .env file")
-		return
+		return errors.New("POSTGRES_URI not set, please set it in your .env file")
 	}
 
-	db, err := sql.Open("postgres", postgresUri)
-	if err != nil {
-		log.Printf("Error connecting to postgres DB: %v", err)
-		return
+	staticDir = os.Getenv("STATIC_DIR")
+	if staticDir == "" {
+		return errors.New("STATIC_DIR not set, please set it in your .env file")
 	}
 
-	store, err := postgres.NewStore(db, []byte("secret"))
-	if err != nil {
-		log.Printf("Error creating postgres session store: %v", err)
-		return
+	return nil
+}
+
+// Connects to our postgres db. Doesn't do much yet.
+func setupPostgresConnection() (*sql.DB, error) {
+	if postgresUri == "" {
+		return nil, fmt.Errorf("seems the postgres connection URI is unset, check you have called loadEnvVars")
 	}
 
+	return sql.Open("postgres", postgresUri)
+}
+
+func setupGinRouter(state *AppState) (*gin.Engine, error) {
 	r := gin.Default()
+
+	store, err := postgres.NewStore(state.db, []byte("secret"))
+	if err != nil {
+		return nil, err
+	}
 
 	r.Use(sessions.Sessions("session", store))
 
@@ -53,16 +74,19 @@ func main() {
 		},
 	})
 
-	staticDir := os.Getenv("STATIC_DIR")
-	if staticDir == "" {
-		log.Println("STATIC_DIR not set, please set it in your .env file")
-	}
-
 	r.Use(static.Serve("/static", static.LocalFile(staticDir, true)))
 
 	// Automatically handles rendering of HTML templates if required.
 	r.LoadHTMLGlob(staticDir + "/*.html")
 
+	return r, nil
+}
+
+// GET /api/email/inbox (Lists emails in inbox)
+// GET /api/email/inbox/search (Fuzzy searches the inbox)
+// DELETE /api/email (Deletes the email address from the database) (CARE SHOULD BE TAKEN SO AS NOT TO DELETE OTHERS)
+// GET /api/email/inbox/<id> (Returns the contents of an email)
+func addRoutes(r *gin.Engine) {
 	r.GET("/temp-mail", func(c *gin.Context) {
 		email := "<temporary_email@example.com>"
 		c.JSON(http.StatusOK, gin.H{
@@ -80,11 +104,32 @@ func main() {
 		api := r.Group("/api")
 		api.GET("/email", emailCreateEndpoint)
 	}
+}
 
-	// GET /api/email/inbox (Lists emails in inbox)
-	// GET /api/email/inbox/search (Fuzzy searches the inbox)
-	// DELETE /api/email (Deletes the email address from the database) (CARE SHOULD BE TAKEN SO AS NOT TO DELETE OTHERS)
-	// GET /api/email/inbox/<id> (Returns the contents of an email)
+func main() {
+	err := loadEnvVars()
+	if err != nil {
+		log.Fatalf("Failed to load environment variables: %v", err)
+	}
+
+	db, err := setupPostgresConnection()
+	if err != nil {
+		log.Printf("Error connecting to postgres DB: %v", err)
+		return
+	}
+
+	state := AppState{
+		db,
+	}
+
+	// This just configures the router etc.
+	r, err := setupGinRouter(&state)
+	if err != nil {
+		log.Fatalf("Something went wrong configuring the Gin router: %v", err)
+	}
+
+	// Add our routes to Gin.
+	addRoutes(r)
 
 	r.Run(":8080")
 }
